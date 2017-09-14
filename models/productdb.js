65e2655288f24b2828f0f08c8e9022442780fb5b
ankit mishra <akm7040@gmail.com>
2017-09-14 08:45:32 +0530

    producdetail api and page implemented

--- models/productdb.js
+++ models/productdb.js
@@ -12,18 +12,70 @@ const productSchema = mongoose.Schema({
   }
 });
 
-const product = module.exports = mongoose.model('product',productSchema);
+const productDetailSchema = mongoose.Schema({
+  type:{
+    type:String,
+    required:true
+  },
+    brand:{
+    type:String,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  },
+  money:{
+    type:String,
+    required:true
+  },
+  image:{
+    type:String,
+    required:true
+  },
+});
 
-module.exports.getProductById = function(id,callback){
-  User.findById(id,callback);
+const product = module.exports = mongoose.model('product',productSchema);
+const productdetail = module.exports = mongoose.model('productdetail',productDetailSchema);
+
+
+module.exports.getProductdetail = function(callback){
+//  console.log(id);
+  productdetail.find({},callback);
 }
 
 module.exports.getProductByName = function(productname,callback){
   const query = {pname:productname}
 //  console.log(username);
-  User.findOne(query,callback);
+  product.findOne(query,callback);
+}
+
+// module.exports.getProductdetail = function(err,data,callback){
+//   if(err){
+//     console.log(err);
+//   }
+//   //console.log(data);
+//   (productdetail.find({}),callback);
+// }
+
+// module.exports.getProductById = function(id,callback){
+//   console.log(id);
+//   product.find({},callback);
+// }
+
+module.exports.getProductByType = function(type,callback){
+  const query = {type:type}
+//  console.log(username);
+  productdetail.find(query,callback);
+}
+
+module.exports.getProductByBrand = function(brand,callback){
+  const query = {brand:brand}
+//  console.log(username);
+  productdetail.find(query,callback);
 }
 
 module.exports.addProduct = function(newProduct, callback){
+  console.log(newProduct);
   newProduct.save(callback);
 }
--- routes/product.js
+++ routes/product.js
@@ -5,25 +5,61 @@ const product = require('../models/productdb');
 
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-  res.send('product page');
-});
+
 
 router.post('/new',(req,res,next)=>{
   let newproduct = new product({
     pname:req.body.pname,
-    bname:req.body.bname,
+    bname:req.body.bname
   });
 
+  product.getProductdetail((err,response)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json({response:response});
+    }
+  })
+  console.log(req.body.pname);
   product.addProduct(newproduct,(err,user)=>{
     if(err){
-      res.json({sucess:false,msg:"failed to register"});
+      res.json({sucess:false,msg:"failed to submit"});
     }else{
-      res.json({sucess:true,msg:"user register"});
+      res.json({sucess:true,msg:"product register"});
     }
   });
   //res.send('users registartion page');
 });
 
+router.get('/',(req,res,next)=>{
+  product.getProductdetail((err,response)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json(response);
+    }
+  })
+})
 
+router.get('/:type',(req,res,next)=>{
+  type = req.params.type;
+  product.getProductByType(type,(err,response)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json(response);
+    }
+  })
+})
+
+router.get('/:type/:brand',(req,res,next)=>{
+  brand = req.params.brand;
+  product.getProductByBrand(brand,(err,response)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json(response);
+    }
+  })
+})
 module.exports = router;
